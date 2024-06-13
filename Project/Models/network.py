c0a1df39bc8bf91dd9db08671086fdaebd9ac959
ElDavido98 <146634840+ElDavido98@users.noreply.github.com>
2024-06-13 18:17:18 +0200

    Update network.py

--- Project/Models/network.py
+++ Project/Models/network.py
@@ -1,94 +1,75 @@
-import torch
-import torch.optim as optim
-from ..Utils.metrics import loss_function
+import statistics
+
+from ..Utils.utils import *
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-low_year_train, max_year_train = 0, 6
-low_hour_train, max_hour_train = 0, 8753
-low_year_val, max_year_val = 0, 0
-low_hour_val, max_hour_val = 0, 4379
-
-
-def EarlyStopping(curr_monitor, old_monitor, count, patience=5, min_delta=0):
-    stop = False
-    if (curr_monitor - old_monitor) <= min_delta:
-        count += 1
-        if count > patience:
-            stop = True
-            return count, stop
-    count = 0
-    return count, stop
-
-
-def lr_schedulers(Net_optimizer):
-    Net_linearLR = optim.lr_scheduler.LinearLR(Net_optimizer, total_iters=5)
-    Net_cos_annLR = optim.lr_scheduler.CosineAnnealingLR(Net_optimizer, T_max=45, eta_min=3.75e-4)
-    return Net_linearLR, Net_cos_annLR
-
-
-def check(Net, Net_func_in: list, time: list, latitude_weights):
-    # Net_func_in = [train_data, utils.low_year_train, utils.max_year_train, lead_time]
-    pred_Net, target_Net = Net(Net_func_in[0], Net_func_in[1], Net_func_in[2], Net_func_in[3], time)
-    loss = loss_function(pred_Net, target_Net, latitude_weights)
-    return loss
-
-
 class Network:
-    def __init__(self, Net, lead_time, train_data, validation_data, lat_weights, name):
+    def __init__(self, Net, lat_weights, name, lead_time):
         self.Net = Net
         self.name = name
+        self.lead_time = lead_time
         self.Net_optimizer = optim.AdamW(self.Net.parameters(), lr=5e-4, weight_decay=1e-5)
         self.Net_linearLR, self.Net_cos_annLR = lr_schedulers(self.Net_optimizer)
         self.low_year_train, self.max_year_train = low_year_train, max_year_train
+        self.low_hour_train, self.max_hour_train = low_hour_train, max_hour_train
         self.low_year_val, self.max_year_val = low_year_val, max_year_val
-        self.lead_time = lead_time
-        self.train_data = train_data
-        self.validation_data = validation_data
+        self.low_hour_val, self.max_hour_val = low_hour_val, max_hour_val
         self.lat_weights = lat_weights
         self.done = False
         self.count = 0
-        self.losses = []
-
-    def reset(self):
-        self.done = False
-        self.count = 0
-        self.losses = []
+        self.loss = None
+        self.validation_losses = []
+        self.previous_validation_losses = self.validation_losses
+        self.pred, self.targ = [], []
 
     def pre_steps(self):
-        self.losses = []
-
-    def step(self, time):
         if not self.done:
-            self.losses.append(check(self.Net, [self.train_data, self.low_year_train, self.max_year_train,
-                                                self.lead_time], time, self.lat_weights))
+            self.count = 0
+            self.loss = None
+            self.validation_losses = []
+            self.previous_validation_losses = self.validation_losses
 
-    def post_steps(self, time, epoch):
-        # Optimization + EarlyStopping
+    def train_step(self, input_list, constants):
+        # input_list = [data, six_hours_ago, twelve_hours_ago, target]
         if not self.done:
-            prev_loss = check(self.Net, [self.validation_data, self.low_year_val, self.max_year_val, 6], time,
+            self.loss = check(self.Net, input_list[0], input_list[1], input_list[2], input_list[3], constants,
                               self.lat_weights)
-            # Optimization
-            loss = torch.FloatTensor(self.losses).to(device)
-            loss.requires_grad = True
+
+    def val_step(self, input_list, constants):
+        # input_list = [data, six_hours_ago, twelve_hours_ago, target]
+        if not self.done:
+            loss = check(self.Net, input_list[0], input_list[1], input_list[2], input_list[3], constants,
+                         self.lat_weights)
+            self.validation_losses.append(loss.item())
+
+    def post_steps(self, epoch):
+        # Optimization
+        if not self.done:
+            loss = self.loss
             self.Net_optimizer.zero_grad()
             loss.mean().backward()
             self.Net_optimizer.step()
-            # EarlyStopping
-            curr_loss = check(self.Net, [self.validation_data, self.low_year_val, self.max_year_val, self.lead_time],
-                              time, self.lat_weights)
-            self.count, self.done = EarlyStopping(curr_loss, prev_loss, self.count)
-            if self.done:
-                print("{}_{} is done".format(self.name, self.lead_time))
-
-        # Learning Rate update
-        # Linear warmup schedule 'if epoch < 5' - Cosine-annealing warmup schedule 'else'
-        if not self.done:
+            # Learning Rate update
+            # Linear warmup schedule if 'epoch < 5' - Cosine-annealing warmup schedule 'else'
             if epoch < 5:
                 self.Net_linearLR.step()
+                self.Net_optimizer.defaults['lr'] = self.Net_linearLR.get_last_lr()
             else:
                 self.Net_cos_annLR.step()
+                self.Net_optimizer.defaults['lr'] = self.Net_cos_annLR.get_last_lr()
 
-        return self.done
+    def stopping(self):
+        if self.previous_validation_losses is None:
+            self.count, self.done = 0, False
+        else:
+            self.count, self.done = EarlyStopping(statistics.mean(self.validation_losses),
+                                                  statistics.mean(self.previous_validation_losses), self.count)
+        if self.done:
+            print("{}_{} is done".format(self.name, self.lead_time))
+
+    def eval_step(self, data, six_hours_ago, twelve_hours_ago, target, constants):
+        p = self.Net(data, six_hours_ago, twelve_hours_ago, target, constants, for_test=1)
+        self.pred.append(p.detach().numpy()), self.targ.append(target.detach().numpy())
